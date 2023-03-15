82593be4c3344bd3b4d2ca4f5dee7048fdd29d56
Prabhdeep Singh Gandhi <prabhdeep.gandhi1807@gmail.com>
2023-03-16 02:18:29 +0530

    Added custom user profile , manager and migrations

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,85 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 # Create your models here.
+
+
+class UserProfileGroup(BaseUserManager):
+    def create_user(self,email,name, password=None):
+        """Create new user profile 
+
+        Args:
+            email (_type_): _description_
+            name (_type_): _description_
+            password (_type_, optional): _description_. Defaults to None.
+
+        Returns:
+            _type_: _description_
+        """
+        
+        if not email:
+            raise ValueError("User must have an email address ")
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name,)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+        
+    def create_superuser(self,email,name,password):
+        """Create a new superuser
+
+        Args:
+            email (_type_): _description_
+            name (_type_): _description_
+            password (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        user=self.create_user(email,name,password)
+        
+        user.is_superuser=True
+        user.is_staff=True
+        user.save(using=self._db)
+        
+        return user
+        
+        
+        
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database model for User 
+
+    Args:
+        AbstractBaseUser (_type_): _description_
+        PermissionsMixin (_type_): _description_
+    """
+    
+    email=models.EmailField( max_length=254, unique=True)
+    name=models.CharField(max_length=254)
+    is_active=models.BooleanField(default=True)
+    is_staff=models.BooleanField(default=False)
+
+    objects=UserProfileGroup()
+        
+    USERNAME_FIELD='email'
+    REQUIRED_FIELDS=['name']
+
+    def get_full_name(self):
+        """ Retrieve full name field"""
+        return self.name 
+    
+
+    def get_short_name(self):
+        """ Retrieve short name field"""
+        return self.name 
+    
+    def __str__(self):
+        """ Retrieve string representation"""
+        return self.email
+    
+     
