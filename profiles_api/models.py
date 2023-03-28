9c2d4e46452d37367b4cbad850a8c01c5af997c3
Prabhdeep Singh Gandhi <prabhdeep.gandhi1807@gmail.com>
2023-03-29 01:35:25 +0530

    Added Profiles Viewset

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -7,16 +7,7 @@ from django.contrib.auth.models import BaseUserManager
 
 class UserProfileGroup(BaseUserManager):
     def create_user(self,email,name, password=None):
-        """Create new user profile 
-
-        Args:
-            email (_type_): _description_
-            name (_type_): _description_
-            password (_type_, optional): _description_. Defaults to None.
-
-        Returns:
-            _type_: _description_
-        """
+        """Create new user profile  """
         
         if not email:
             raise ValueError("User must have an email address ")
@@ -30,16 +21,7 @@ class UserProfileGroup(BaseUserManager):
         return user
         
     def create_superuser(self,email,name,password):
-        """Create a new superuser
-
-        Args:
-            email (_type_): _description_
-            name (_type_): _description_
-            password (_type_): _description_
-
-        Returns:
-            _type_: _description_
-        """
+        """Create a new superuser        """
         user=self.create_user(email,name,password)
         
         user.is_superuser=True
